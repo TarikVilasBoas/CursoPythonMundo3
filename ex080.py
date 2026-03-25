635cd7ea9c31771795d4a41e9037facce73fe914
Tarik Vilas Boas <tarikvbtgamer@gmail.com>
2026-03-25 10:49:08 -0300

    Update ex080.py

--- ex080.py
+++ ex080.py
@@ -1,4 +1,10 @@
-num = []
-for i in range(5):
-    n = int(input('Digite um numero: '))
-print(num)
+numeros = []
+for n in range(1):
+    num = int(input('Digite um numero: '))
+    if not numeros:
+        numeros.append(num)
+        print(f'Iten adicionado na posição {n}')
+    if num > numeros[0]:
+        numeros.insert(1,num)
+    elif num < numeros[0]:
+        numeros.insert(0, num)
