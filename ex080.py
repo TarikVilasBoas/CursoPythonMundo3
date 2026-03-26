27d7d9f912589a2fef9f60f4b138f5101f514bd4
Tarik Vilas Boas <tarikvbtgamer@gmail.com>
2026-03-26 08:58:53 -0300

    Update ex080.py

--- ex080.py
+++ ex080.py
@@ -1,10 +1,16 @@
 numeros = []
-for n in range(1):
+for n in range(0,5):
     num = int(input('Digite um numero: '))
-    if not numeros:
+    if n == 0 or num > numeros[-1]:
         numeros.append(num)
-        print(f'Iten adicionado na posição {n}')
-    if num > numeros[0]:
-        numeros.insert(1,num)
-    elif num < numeros[0]:
-        numeros.insert(0, num)
+    else:
+        pos = 0
+        while pos < len(numeros):
+            if n <= numeros[pos]:
+                numeros.insert(pos, n)
+                break
+            pos += 1
+print('-' * 30)
+print(f'Os valores digitados foram {numeros}')
+
+
