9692a3e0f8b9a80bce678e3759e94538b88ae4f6
Tarik Vilas Boas <tarikvbtgamer@gmail.com>
2026-03-29 10:57:30 -0300

    Create ex088.py

---
+++ ex088.py
@@ -0,0 +1,16 @@
+from random import randint
+from time import sleep
+numeros = []
+pc = []
+print(f'{'JOGO DA LOTERIA':*^50}')
+print()
+j = int(input('Quantos jogos voce quer que eu sorteie? '))
+for e in range(j):
+    for n in range(6):
+        pc.append(randint(0,61))
+    numeros.append(pc[:])
+    pc.clear()
+print('='*50)
+for n in range(len(numeros)):
+    print(f'O {n+1}o numero sugerido é = {numeros[n]}')
+    sleep(0.7)
