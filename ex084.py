3f6194988e5e110f470c9f7713f93fc4efd089a8
Tarik Vilas Boas <tarikvbtgamer@gmail.com>
2026-03-30 09:47:17 -0300

    Update ex084.py
    melhoria no codigo

--- ex084.py
+++ ex084.py
@@ -1,26 +1,30 @@
 cadastro = []
 dados = []
-maisp = []
-menosp = []
-contp = mais = menos = 0
+mais = menos = 0
 while True:
     us = ' '
     dados.append(str(input('Nome: ')))
-    dados.append(int(input('Peso: ')))
+    dados.append(float(input('Peso: ')))
+    if len(cadastro) == 0:
+        mais = menos = dados[1]
+    else:
+        if dados[1] > mais:
+            mais = dados[1]
+        if dados[1] < menos:
+            menos = dados[1]
     cadastro.append(dados[:])
     dados.clear()
-    contp += 1
     while us not in 'SsNn':
         us = str(input('Deseja continuar? [S/n]: '))
     if us in 'Nn':
         break
-for c in cadastro:
-    if c[1] >= 100:
-        maisp.append(c[0])
-        mais = c[1]
-    elif c[1] <= 70:
-        menosp.append(c[0])
-        menos = c[1]
-print(f'Foram digitadas {contp} pessoas')
-print(f'O maior peso foi {mais}kg Foram das pessoas: {maisp}')
-print(f'O menor peso foi {menos}kg e são das pessoas:  {menosp}')
+print(f'Foram digitadas {len(cadastro)} pessoas')
+print(f'O maior peso foi {mais}kg, peso de ',end=' ')
+for p in cadastro:
+    if p[1] == mais:
+        print(f'{p[0]} ',end=', ')
+print()
+print(f'O menor peso foi {menos}kg peso de ',end=' ')
+for p in cadastro:
+    if p[1] == menos:
+        print(f'{p[0]} ',end=', ')
