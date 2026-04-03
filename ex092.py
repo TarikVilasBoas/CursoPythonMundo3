9a5510b31251a459bdb3849d553ab6f11f75e2a1
Tarik Vilas Boas <tarikvbtgamer@gmail.com>
2026-04-03 10:31:53 -0300

    Create ex092.py

---
+++ ex092.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+funcionario = {}
+funcionario['nome'] = str(input('Nome: '))
+ano = int(input('Ano de nascimento: '))
+funcionario['idade'] = datetime.now().year - ano
+ctps = int(input('N carteira de trabalho (0 nao tem): '))
+if ctps > 0:
+    funcionario['contratacao'] = int(input('Ano de contratação: '))
+    funcionario['salario'] = float(input('Salario: '))
+    aposent = funcionario['contratacao'] - ano + 35
+    funcionario['aposentadoria'] = aposent
+for i, v in funcionario.items():
+    print(f'O {i} é = {v}')
