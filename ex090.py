28f2d6e4bb7caee770bcba0492be0e3083437168
Tarik Vilas Boas <tarikvbtgamer@gmail.com>
2026-04-09 08:50:32 -0300

    Update ex090.py

--- ex090.py
+++ ex090.py
@@ -4,7 +4,9 @@ aluno['Media'] = float(input(f'Media de {aluno['Nome']}: '))
 for n, m in aluno.items():
     print(f'O {n} é {m}')
 print('A situação é : ',end='')
-if aluno['Media'] >= 6:
+if aluno['Media'] >= 7:
     print('Aprovado')
+elif aluno['Media'] >= 5:
+    print('Recuperação')
 else:
     print('Reprovado')
