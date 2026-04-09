c9890cc7e97ff2b2f66f4d6c2a00ccc121c86ab3
Tarik Vilas Boas <tarikvbtgamer@gmail.com>
2026-04-09 08:58:54 -0300

    Update ex094.py

--- ex094.py
+++ ex094.py
@@ -1,29 +1,30 @@
 dados = {}
 pessoas = []
-mul = []
 cont = media = soma = 0
 while True:
-    dados[f'nome'] = str(input('Nome: '))
+    resp = ' '
+    dados['nome'] = str(input('Nome: '))
     dados['sexo'] = 'a'
     while dados['sexo'] not in 'MmFf':
-        dados[f'sexo'] = str(input('Sexo [M/f]: ')).strip()[0]
-    dados[f'idade'] = int(input('Idade: '))
-    if dados['sexo'] in 'Ff':
-        mul.append(dados['nome'])
-        dados['mulher'] = mul.copy()
+        dados['sexo'] = str(input('Sexo [M/f]: ')).strip()[0]
+    dados['idade'] = int(input('Idade: '))
+    soma += dados['idade']
     pessoas.append(dados.copy())
     cont += 1
-    resp = str(input('Deseja continuar? [S/n]: ')).strip()[0]
+    while resp not in 'SsNn':
+        resp = str(input('Deseja continuar? [S/n]: ')).strip()[0]
     if resp in 'Nn':
         break
-for c in pessoas:
-    soma += c['idade']
 media = soma / cont
-
 print('-'*30)
 print(f'- O grupo tem {cont} pessoas')
 print(f'- A media de idade entre eles é de {media:.2f}')
-print(f'- As mulheres cadastradas foram {dados['mulher']}')
-print(f'As pessoas com idade acima da media de {media:.2f} sao:')
-
-print('FIM')
+print(f'- As mulheres cadastradas foram ',end=' ')
+for i, k in enumerate(pessoas):
+    if k['sexo'] in 'Ff':
+        print(f'{k['nome']}',end=', ')
+print()
+print(f'As pessoas com idade acima da media de {media:.2f} sao: ',end='')
+for i, k in enumerate(pessoas):
+    if k['idade'] > media:
+        print(f'{k['nome']}',end='')
