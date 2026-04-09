82b48e25dc3b73abdb081f4a5a9a2d99a34dc5ec
Tarik Vilas Boas <tarikvbtgamer@gmail.com>
2026-04-09 08:58:49 -0300

    Update ex093.py

--- ex093.py
+++ ex093.py
@@ -1,16 +1,18 @@
 dados = {}
 gols = []
 dados['nome'] = str(input('Nome do jogador: '))
-part = int(input('Quantas partidas ele jogou? '))
+part = int(input(f'Quantas partidas o {dados["nome"]} jogou? '))
 for q in range(part):
     gols.append(int(input(f'Quantos gols na {q+1} partida? ')))
-dados['gols'] = gols.copy()
+dados['gols'] = gols[:]
 dados['total'] = sum(gols)
 print('=*'*30)
 for i, v in dados.items():
     print(f'O campo {i} tem o valor {v}.')
 print('=*'*30)
-print(f'O jogador {dados['nome']} jogou {part} partidas')
+print(f'O jogador {dados['nome']} jogou {len(gols)} partidas')
 for i in range(part):
     print(f'=> Na partida {i+1} ele fez {gols[i]} gols')
 print(f'Foi um total de {dados['total']} gols')
+
+print(dados)
