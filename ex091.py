025420961320c9700b0dd5217c263bdf91ad5e48
Tarik Vilas Boas <tarikvbtgamer@gmail.com>
2026-04-09 08:50:52 -0300

    Update ex091.py

--- ex091.py
+++ ex091.py
@@ -1,8 +1,15 @@
 from random import randint
+from time import sleep
+from operator import itemgetter
 dados = {}
+rank = {}
 for j in range(1,5):
     dados[f'Jogador{j}'] = randint(1,6)
 for i,d in dados.items():
     print(f'O {i} tirou {d}')
-
-print(dados)
+    sleep(0.5)
+print('-_'*30)
+rank = sorted(dados.items(), key=itemgetter(1), reverse=True)
+for i, d in enumerate(rank):
+    print(f'Em {i+1} lugar o {d[0]} com {d[1]}')
+    sleep(0.6)
