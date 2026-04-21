f1e9c5ebea2e1e54b496e5f02f032fa6a2630028
Tarik Vilas Boas <tarikvbtgamer@gmail.com>
2026-04-21 10:11:12 -0300

    Update ex095.py

--- ex095.py
+++ ex095.py
@@ -1,6 +1,7 @@
 dados = {}
 gols = []
 jogadores = []
+cont = 0
 while True:
     resp = ' '
     dados['nome'] = str(input('Nome do jogador: '))
@@ -10,13 +11,28 @@ while True:
     dados['gols'] = gols[:]
     dados['total'] = sum(gols)
     jogadores.append(dados.copy())
+    gols.clear()
     dados.clear()
+    cont += 1
     while resp not in 'SsNn':
         resp = str(input('Deseja continuar? S/n: '))
     if resp in 'Nn':
         break
 print('='*40)
-print(f'{'Cod':<}{'Nome':^10}{'Gols'}{'Total de Gols':>20}')
+print(f'{'Cod':<}{'Nome':^10}{'Gols'}{'Total':>20}')
 print('-'*40)
 for i, v in enumerate(jogadores):
-    print(f'{i} {v['nome']:^10} {v['gols']}  {v['total']:>10}')
+    print(f'{i} {v['nome']:^10} {v['gols']}  {v['total']:>15}')
+while True:
+    x = ' '
+    print('='*30)
+    x = int(input('Mostrar dados de qual jogador? (999 para parar): '))
+    if x == 999:
+        break
+    if x >= len(jogadores):
+        print(f'Erro: Nao existe jogador com codigo {x}')
+    else:
+        print('-'*30)
+        print(f'Levantamento do jogador {jogadores[x]['nome']}:')
+        for i,v in enumerate(jogadores[x]['gols']):
+            print(f'No jogo {i+1} ele fez {v} gols  ')
