a828c66e1d24dd6fa3dd5e267452812e2b46e826
Tarik Vilas Boas <tarikvbtgamer@gmail.com>
2026-04-29 08:56:58 -0300

    Update ex099.py

--- ex099.py
+++ ex099.py
@@ -1,9 +1,16 @@
+from time import sleep
 def maior(*num):
+    t = len(num)
     m = max(num)
     print('-'*25)
-    print(f'Os numeros foram {num}')
+    print(f'Ao todo foram {t} numeros, {num}')
     print(f'E o maior foi o {m}')
 
 
 maior(1, 3, 5, 9)
+sleep(1)
 maior(36,0,15,8,25,)
+sleep(1)
+maior(8,5,1)
+sleep(1)
+maior(65, 100, 99, 154, 35, 15, 38)
