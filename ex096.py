10c32fd6d1f72705d45ec90ae09375443109db6e
Tarik Vilas Boas <tarikvbtgamer@gmail.com>
2026-04-29 08:35:54 -0300

    Update ex096.py

--- ex096.py
+++ ex096.py
@@ -1,11 +1,11 @@
 def area(l, c):
     t = l * c
-    print(f'A área de um terreno de {l} * {c} é de: {t}')
+    print(f'A área de um terreno de {l} * {c} é de: {t}m2')
 
-
+#Programa principal
 print('Controle de terrenos')
 print('-'*25)
-a = float(input('Largura do terreno (m): '))
-b = float(input('Comprimento do terreno (m): '))
+l = float(input('Largura do terreno (m): '))
+c = float(input('Comprimento do terreno (m): '))
 
-area(a,b)
+area(l,c)
