6aec2fe77a42309d4866d01f32a012341350a26b
Tarik Vilas Boas <tarikvbtgamer@gmail.com>
2026-04-29 08:36:12 -0300

    Update ex098.py

--- ex098.py
+++ ex098.py
@@ -1,29 +1,30 @@
 from time import sleep
-def contador(a, b, c):
-    valor = []
-    if a < b:
-        print('-'* 40)
-        print(f'{a}',end=', ')
-        while a <= b:
-            a += c
-            valor.append(str(a))
-        print(f'{', '.join(valor)}')
-        print('-'*40)
-    elif a > b:
-        print('-'*40)
-        print(f'{a}',end=', ')
-        while a >= b:
-            a += c
-            valor.append(str(a))
-        print(f'{', '. join(valor)}')
-        print('-'*40)
-
-
-contador(1, 10, 1)
-contador(10, 1, -2)
+def contador(i, f, p):
+    if p < 0:
+        p *= -1
+    if p == 0:
+        p = 1
+    print(f'Contagem de {i} ate {f} de {p} em {p}')
+    sleep(1)
+    if i < f:
+        cont = i
+        while cont <= f:
+            print(f'{cont}', end=' ')
+            sleep(0.5)
+            cont += p
+        print('Fim')
+    else:
+        cont = i
+        while cont >= f:
+            print(f'{cont}', end=' ')
+            sleep(0.5)
+            cont -= p
+        print('Fim')
+contador(1, 10, 2)
+contador(10, 1, 2)
 print('Sua vez de definir os parametros da contagem.')
-a = int(input('Inicio: '))
-b = int(input('Fim: '))
-c = int(input('Passo: '))
-contador(a, b, c)
+i = int(input('Inicio: '))
+f = int(input('Fim: '))
+p = int(input('Passo: '))
+contador(i, f, p)
 
