f0f865fad8bc179b3f718d68a2f9ca7767584a5e
Tarik Vilas Boas <tarikvbtgamer@gmail.com>
2026-05-24 10:38:36 -0300

    Update ex101.py

--- ex101.py
+++ ex101.py
@@ -1,14 +1,14 @@
-from datetime import date
 
 def voto(ano):
+    from datetime import date        # A biblioteca pode ser chamada dentro da funçao e ira funcionar so nela
+    # Isso ajuda na reduçao do espaço usado na memoria
     idade = date.today().year - ano
-    if idade < 18:
+    if idade < 16:
         return f'Com {idade} anos nao é obrigado a votar'
-    elif idade >= 18 and idade <= 65 :
+    elif idade > 16 and idade <= 65 :
         return f'Com {idade} voce é obrigado a votar'
     else:
         return f'Com {idade} Seu voto é opcional'
-
 
 nasc = int(input('Em que ano voce nasceu? '))
 print(voto(nasc))
